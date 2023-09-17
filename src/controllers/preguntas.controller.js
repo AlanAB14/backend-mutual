15d2cfb090d253f5c3f1cc796fcfa0e9c1ec0174
Alan Bersia <alanbersia@gmail.com>
2023-09-16 23:14:57 -0300

    Corrijo preguntas

--- src/controllers/preguntas.controller.js
+++ src/controllers/preguntas.controller.js
@@ -2,7 +2,7 @@ import { pool } from './../db.js'
 
 export const getPreguntas = async (req, res) => {
     try {
-        const [rows] = await pool.query('SELECT * FROM preguntas WHERE id = ?', [req.params.id])
+        const [rows] = await pool.query('SELECT * FROM preguntas WHERE categoria_id = ?', [req.params.id])
         if (rows.length <= 0) return res.status(404).json({
             message: 'Preguntas not found'
         })
