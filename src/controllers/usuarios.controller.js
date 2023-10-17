f58a674a99d4d9df1980880458d3551f26be1b53
Alan Bersia <alanbersia@gmail.com>
2023-10-17 13:18:40 -0300

    Corrijo error en patch password

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -83,10 +83,12 @@ export const loginUsuario = async (req, res) => {
 
 export const updateUsuario = async (req, res) => {
     const { id } = req.params
-    const { password, super_admin } = req.body
+    let { password, super_admin } = req.body
     try {
-        const hashPass = bcrypt.hashSync(password, 10)
-        const result = await pool.query('UPDATE usuarios set password = IFNULL(?, password), super_admin = IFNULL(?, super_admin) WHERE id = ?', [hashPass, super_admin, id])
+        if ( password ) {
+            password = bcrypt.hashSync(password, 10)
+        }
+        const result = await pool.query('UPDATE usuarios set password = IFNULL(?, password), super_admin = IFNULL(?, super_admin) WHERE id = ?', [password, super_admin, id])
         if (result.affectedRows === 0) return res.status(404).json({
             message: 'Servicio not found'
         })
